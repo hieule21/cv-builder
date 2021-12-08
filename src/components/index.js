7960070eaa2cdf1a8687b8cce9e5f69f188ed00f
Hieu Le <hieule@Hieus-MacBook-Air.local>
2021-12-08 15:35:00 -0600

    Updates

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Login } from "./login/login";
-export { default as Register } from "./register/register";
-export { default as Home } from "./home/home";
-export { default as Project } from "./project/project";
